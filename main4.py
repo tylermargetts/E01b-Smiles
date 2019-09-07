2638da47d6e9b90c5892472fc5aef4c29637514e
Tyler Margetts <tmargett@iu.edu>
2019-09-07 18:17:47 -0400

    Update main4.py

--- main4.py
+++ main4.py
@@ -17,11 +17,11 @@ for x in range(100,800,150):
         #start at 100, go to 599, counting by 150
         for y in range(100,600,150):
                 face_x,face_y = (x,y)
-                smile_x,smile_y = (face_x + 0,face_y - 0)
-                eye1_x,eye1_y = (face_x - 0,face_y + 0) 
-                eye2_x,eye2_y = (face_x + 0,face_y + 0)
-                catch1_x,catch1_y = (face_x - 0,face_y + 0) 
-                catch2_x,catch2_y = (face_x + 0,face_y + 0) 
+                smile_x,smile_y = (face_x + 0,face_y - 15)
+                eye1_x,eye1_y = (face_x - 35,face_y + 35) 
+                eye2_x,eye2_y = (face_x + 35,face_y + 35)
+                catch1_x,catch1_y = (face_x - 30,face_y + 45) 
+                catch2_x,catch2_y = (face_x + 40,face_y + 45) 
 
 
                 # Draw the smiley face:
@@ -31,8 +31,8 @@ for x in range(100,800,150):
                 arcade.draw_circle_outline(face_x, face_y, 100, open_color.black,4)
 
                 #(x,y,width,height,color)
-                arcade.draw_ellipse_filled(eye1_x,eye1_y,15,25,open_color.black)
-                arcade.draw_ellipse_filled(eye2_x,eye2_y,15,25,open_color.black)
+                arcade.draw_ellipse_filled(eye1_x,eye1_y,30,50,open_color.black)
+                arcade.draw_ellipse_filled(eye2_x,eye2_y,30,50,open_color.black)
                 arcade.draw_circle_filled(catch1_x,catch1_y,3,open_color.gray_2)
                 arcade.draw_circle_filled(catch2_x,catch2_y,3,open_color.gray_2)
 
